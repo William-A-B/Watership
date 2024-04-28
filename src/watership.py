aafd93aa98deec0a89225377b51d3a3c3a0e91b1
William-A-B <104766801+William-A-B@users.noreply.github.com>
2024-04-28 19:37:24 +0100

    Create simple hello slash command

---
+++ src/watership.py
@@ -0,0 +1,17 @@
+import os
+import discord
+import dotenv
+
+dotenv.load_dotenv()
+
+bot = discord.Bot()
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is ready and online!")
+
+@bot.slash_command(name="hello", description="Say hello to the bot")
+async def hello(ctx: discord.ApplicationContext):
+    await ctx.respond("Hey!")
+
+bot.run(os.getenv("TOKEN"))
