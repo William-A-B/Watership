b64ad51dce55b64d4c936342d7fe9bc0cd209ec7
William-A-B <104766801+William-A-B@users.noreply.github.com>
2024-04-30 11:38:41 +0100

    update event listeners

--- src/watership.py
+++ src/watership.py
@@ -4,14 +4,27 @@ import dotenv
 
 dotenv.load_dotenv()
 
-bot = discord.Bot()
+bot = discord.Bot(intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
     print(f"{bot.user} is ready and online!")
 
+
+@bot.event
+async def on_member_join(member):
+    await member.send(f"Welcome to the server, {member.mention}! Enjoy your stay here.")
+    member.give_role(3)
+
+
+
 @bot.slash_command(name="hello", description="Say hello to the bot")
 async def hello(ctx: discord.ApplicationContext):
     await ctx.respond("Hey!")
+
+@bot.event
+async def on_message(message):
+    if (message.content == "ping"):
+        await message.delete()
 
 bot.run(os.getenv("TOKEN"))
